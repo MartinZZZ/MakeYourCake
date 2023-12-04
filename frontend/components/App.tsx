8df4fb1b89ac3b18681b11732abcf598ae3958f7
ktibenska <katkatib7@gmail.com>
2023-12-05 00:51:25 +0100

    pridany velmi jednoduchy o nas

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,3 +1,25 @@
+import {Box, Button, Typography} from "@mui/material";
+
 export const App = () => {
-  return <h1>O nas</h1>
+    return (
+        <>
+            {/*<h1>O nás</h1>*/}
+            <Typography variant="h2" padding='10vh' align='center'>
+                Vitajte na Make Your Cake!
+            </Typography>
+
+            <Typography variant="h5" display="block" >
+                Vyskladajte si vlastnú tortu podľa Vašich predstáv, alebo si vyberte hotový dizajn z našej galérie.
+                <br/>
+                <br/>
+                Vyberte si Vaše vysnívané príchute cesta, plnky aj polevy rýchlo a jednoducho.
+                <br/>
+                Všetky torty ponúkame aj bezlepkové, bezgluténové a vegánske.
+            </Typography>
+
+
+        </>
+
+
+    )
 }
