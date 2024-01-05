cf6e1275e93e51d84a2d27ce1011b6d4899a51f3
m4rtin7 <m.zavadzan@gmail.com>
2024-01-05 21:09:58 +0100

    dragg to image

--- frontend/components/doplnky/Dragg.tsx
+++ frontend/components/doplnky/Dragg.tsx
@@ -1,37 +1,58 @@
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import { useState } from 'react'
 import Draggable from 'react-draggable'
 import { Torta } from '../Torta'
+import html2canvas from 'html2canvas'
+
+const handleDownloadImage = async () => {
+  const element = document.getElementById('draggArea')
+  const canvas = await html2canvas(element)
+  const [_, base64Image] = canvas.toDataURL().split(';base64,')
+  console.log(base64Image)
+  // let link = document.createElement('a')
+
+  // link.href = base64Image
+  // link.download = 'downloaded-image.jpg'
+
+  // document.body.appendChild(link)
+  // link.click()
+  // document.body.removeChild(link)
+}
 
 export const Dragg = () => {
   const [position, setPosition] = useState({ x: 100, y: 100 })
   console.log(position)
+
   return (
-    <Box
-      sx={{
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-      }}
-    >
-      <Box sx={{ m: 4 }}>
-        <Torta />
-      </Box>
-      <Draggable
-        onStop={(_, data) => setPosition({ x: data.x, y: data.y })}
-        defaultPosition={position}
-        bounds="parent"
+    <>
+      <Box
+        id="draggArea"
+        sx={{
+          width: '100%',
+          height: '100%',
+          position: 'relative',
+        }}
       >
-        <div
-          style={{
-            zIndex: 100,
-            position: 'relative',
-            width: 'fit-content',
-          }}
+        <Box sx={{ m: 4 }}>
+          <Torta />
+        </Box>
+        <Draggable
+          onStop={(_, data) => setPosition({ x: data.x, y: data.y })}
+          defaultPosition={position}
+          bounds="parent"
         >
-          XD
-        </div>
-      </Draggable>
-    </Box>
+          <div
+            style={{
+              zIndex: 100,
+              position: 'relative',
+              width: 'fit-content',
+            }}
+          >
+            XD
+          </div>
+        </Draggable>
+      </Box>
+      <Button onClick={handleDownloadImage}>Download</Button>
+    </>
   )
 }
