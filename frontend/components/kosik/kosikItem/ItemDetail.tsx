1c49be89aa5560cc320243d61b728b051f1b64d1
karkub <karinka7117@gmail.com>
2024-01-06 19:42:48 +0100

    prepojenie ponuky s kosikom
    - pridanie vyberu obmedzeni aj do ponuky

--- frontend/components/kosik/kosikItem/ItemDetail.tsx
+++ frontend/components/kosik/kosikItem/ItemDetail.tsx
@@ -1,6 +1,8 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import React from "react";
 import {LabelTextPair} from "../../LabelTextPair";
+import { getTranslation } from '../../../helpers/getTranslation'
+
 
 export function ItemDetail(vlastny, item) {
 
@@ -17,6 +19,15 @@ export function ItemDetail(vlastny, item) {
             {Object.entries(item.properties).map(([key, value]) => (
                 <LabelTextPair key={key} label={key} text={value?.toString()}/>
             ))}
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                <Typography>Obmedzenia:</Typography>
+                {item.restrictions.map((restriction, index) => (
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                        {getTranslation(restriction)}
+                        {index < item.restrictions.length - 1 && ", "}
+                    </Typography>        
+                ))}
+            </Box>
         </Box>
     )
 }
