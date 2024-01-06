c7990921f98f096b505c4972461574622e897e58
Karin Kubinová <95253218+karkub@users.noreply.github.com>
2024-01-06 16:25:23 +0100

    ponuka (#9)
    - store namiesto url
    - pridanie dalsich tort
    - filtre podla kategorie / obmedzeni

--- frontend/pages/ponuka/[id].tsx
+++ frontend/pages/ponuka/[id].tsx
@@ -1,5 +1,10 @@
+
+import { RootState } from '../../redux/store'
+import React from 'react'
+import router from 'next/router'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import {useRouter} from 'next/router'
-import {useState, useEffect} from 'react'
 import {
     Box,
     Button,
@@ -16,213 +21,247 @@ import {
     Typography,
 } from '@mui/material'
 import Carousel from 'react-material-ui-carousel'
-import {ShoppingCartOutlined, ShoppingCart} from '@mui/icons-material'
-import React from 'react'
+import { ShoppingCartOutlined, ShoppingCart } from '@mui/icons-material'
+import { setFinalPrice, setSize, setFlavour } from '../../redux/features/cakeMenuSlice'; 
 import {AddToKosik} from "../../components/kosik/UpdateKosik";
 
+
+const Restrictions = ({restrictions}) => {
+    if (restrictions.length > 0) {
+        //TODO radio buttons
+        const restrictionOption = restrictions.map((r) => (
+                <Typography>
+                    {r}
+                {/* <RadioGroup
+                  name="controlled-radio-buttons-group"
+                  value={restrictions}
+                  onChange={() => {}}
+                >
+                  <FormControlLabel
+                    value={r}
+                    control={<Radio />}
+                    label={r}
+                  /> 
+                  </RadioGroup>*/}
+                </Typography>
+        ))
+        return (
+            <Typography variant="h6" my={1}>
+                Obmedzenia:
+                {restrictionOption}
+            </Typography>
+        )
+    }
+  } 
+
 const CakeDetail = () => {
-    const router = useRouter()
-    const {id, name, description, price: priceFromUrl} = router.query
-
-    const priceFromString = Array.isArray(priceFromUrl)
-        ? priceFromUrl[0]
-        : priceFromUrl
-    const initialPrice = parseFloat(priceFromString) || 0
-    const [additionalPrice, setAdditionalPrice] = useState(0)
-    const [totalPrice, setTotalPrice] = useState(initialPrice)
-
-    // const selectedId = useAppSelector((state) => state.cakeInfoReducer.id)
-    // const selectedName = useAppSelector((state) => state.cakeInfoReducer.name)
-    // const selectedDescription = useAppSelector((state) => state.cakeInfoReducer.description)
-    // const selectedPrice = useAppSelector((state) => state.cakeInfoReducer.price)
-    // const dispatch = useDispatch<AppDispatch>()
     useEffect(() => {
-        setTotalPrice(initialPrice + additionalPrice)
-    }, [additionalPrice, initialPrice])
+        dispatch(setFinalPrice(price))
+        dispatch(setSize(12))
+        dispatch(setFlavour(''))
+    }, [])
 
-    const [size, setSize] = useState('12')
-    const [flavour, setFlavour] = useState('')
+  const dispatch = useDispatch();
 
-    const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.persist()
-        const newSize = (event.target as HTMLInputElement).value
-        setSize((prevSize) => {
-            setAdditionalPrice((prevAdditionalPrice) => {
-                if (newSize === '17') {
-                    return 20
-                } else {
-                    return 0
-                }
-            })
-            return newSize
-        })
-    }
-    const handleFlavourChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.persist()
-        const newFlavour = (event.target as HTMLInputElement).value
-        setFlavour(newFlavour)
-    }
+  const { id, name, description, price, finalPrice, size, flavour, restrictions} = useSelector((state: RootState) => state.cakeMenu)
+  const [flavourError, setFlavourError] = useState(false)
+  const [open, setOpen] = React.useState(false)
 
-    const [open, setOpen] = React.useState(false)
-    const handleClickOpen = () => {
-        setOpen(true)
-        AddToKosik(false, {'name': name, 'size': size, 'flavour': flavour}, null, id, priceFromString)
+  const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = (event.target as HTMLInputElement).value
+    dispatch(setSize(Number(newSize)))
+    if (newSize === '17') {
+        dispatch(setFinalPrice(price+20))
+    } else if (newSize === '25') {
+        dispatch(setFinalPrice(price+50))
+    } else {
+        dispatch(setFinalPrice(price))
     }
-    const handleClose = () => {
-        setOpen(false)
-    }
-    const handleToCart = () => {
-        setOpen(false)
-        router.push({
-            pathname: '/kosik'
-            //     query: {id: id, name: name, description: description, price: totalPrice}
-        })
-    }
+  }
 
-    return (
-        <Box my={5}>
-            <Typography variant="h4">{name}</Typography>
+  const handleFlavourChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newFlavour = (event.target as HTMLInputElement).value
+    dispatch(setFlavour(newFlavour))
+    setFlavourError(false)    
+  }
 
-            <Box mt={3}>
-                <Grid container spacing={1}>
-                    <Grid item sm={12} md={6}>
-                        <Card sx={{maxWidth: 500}}>
-                            <Carousel
-                                autoPlay={false}
-                                indicators={true}
-                                indicatorContainerProps={{
-                                    style: {
-                                        position: 'absolute',
-                                        bottom: 16,
-                                        textAlign: 'center',
-                                    },
-                                }}
-                                navButtonsAlwaysVisible={true}
-                                navButtonsProps={{
-                                    style: {
-                                        backgroundColor: 'transparent',
-                                        color: 'black',
-                                        borderRadius: 0,
-                                    },
-                                }}
-                            >
-                                {[0, 1].map((index) => (
-                                    <CardMedia
-                                        key={index}
-                                        component="img"
-                                        height="500"
-                                        image={`/static/images/cakes/${id}/${index}.png`}
-                                        alt={`Cake ${name} image ${index + 1}`}
-                                    />
-                                ))}
-                            </Carousel>
-                        </Card>
-                    </Grid>
-                    <Grid item sm={12} md={6}>
-                        <Typography variant="h5" my={1}>
-                            {description}
+  const handleClickOpen = () => {
+    if (!flavour) {
+      setFlavourError(true)
+      return
+    }
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+      <Box my={5}>
+          <Typography variant="h4">{name}</Typography>
+
+      <Box mt={3}>
+        <Grid container spacing={1}>
+          <Grid item sm={12} md={6}>
+            <Card sx={{ maxWidth: 500 }}>
+              <Carousel
+                autoPlay={false}
+                indicators={true}
+                indicatorContainerProps={{
+                  style: {
+                    position: 'absolute',
+                    bottom: 16,
+                    textAlign: 'center',
+                  },
+                }}
+                navButtonsAlwaysVisible={true}
+                navButtonsProps={{
+                  style: {
+                    backgroundColor: 'transparent',
+                    color: 'black',
+                    borderRadius: 0,
+                  },
+                }}
+              >
+                {[0, 1].map((index) => (
+                  <CardMedia
+                    key={index}
+                    component="img"
+                    height="500"
+                    image={`/static/images/cakes/${id}/${index}.png`}
+                    alt={`Cake ${name} image ${index + 1}`}
+                  />
+                ))}
+              </Carousel>
+            </Card>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Typography variant="h5" my={1}>
+              {description}
+            </Typography>
+            <Box>
+              <FormControl>
+                <Typography variant="h6" my={1}>
+                  Veľkosť
+                <RadioGroup
+                  name="controlled-radio-buttons-group"
+                  value={size}
+                  onChange={handleSizeChange}
+                >
+                  <FormControlLabel
+                    value="12"
+                    control={<Radio />}
+                    label="12cm (8-12 porcií)"
+                  />
+                  <FormControlLabel
+                    value="17"
+                    control={<Radio />}
+                    label={
+                        <Typography>
+                        17cm (12-18 porcií)  <i>+20€</i>
                         </Typography>
-                        <Box>
-                            <FormControl>
-                                <Typography variant="h6" my={1}>
-                                    Veľkosť
-                                </Typography>
-                                <RadioGroup
-                                    name="controlled-radio-buttons-group"
-                                    value={size}
-                                    onChange={handleSizeChange}
-                                >
-                                    <FormControlLabel
-                                        value="12"
-                                        control={<Radio/>}
-                                        label="12cm (8-12 porcií)"
-                                    />
-                                    <FormControlLabel
-                                        value="17"
-                                        control={<Radio/>}
-                                        label="17cm (12-18 porcií) +20€"
-                                    />
-                                </RadioGroup>
-                            </FormControl>
-                        </Box>
-                        <Box>
-                            <FormControl>
-                                <Typography variant="h6" my={1}>
-                                    Príchuť
-                                </Typography>
-                                <RadioGroup
-                                    name="controlled-radio-buttons-group"
-                                    value={flavour}
-                                    onChange={handleFlavourChange}
-                                >
-                                    <FormControlLabel
-                                        value="chocolate"
-                                        control={<Radio/>}
-                                        label="čokoládová"
-                                    />
-                                    <FormControlLabel
-                                        value="vanilla"
-                                        control={<Radio/>}
-                                        label="vanilková"
-                                    />
-                                    <FormControlLabel
-                                        value="strawberry"
-                                        control={<Radio/>}
-                                        label="jahodová"
-                                    />
-                                    <FormControlLabel
-                                        value="coffee"
-                                        control={<Radio/>}
-                                        label="kávová"
-                                    />
-                                </RadioGroup>
-                            </FormControl>
-                        </Box>
-                        <Box
-                            display="flex"
-                            justifyContent="space-between"
-                            alignItems="center"
-                            m={2}
-                        >
-                            <Typography variant="h6" my={2}>
-                                Cena: {totalPrice}€
-                            </Typography>
-                            <Button
-                                variant="contained"
-                                endIcon={<ShoppingCartOutlined/>}
-                                onClick={handleClickOpen}
-                            >
-                                {' '}
-                                Kúpiť
-                            </Button>
-                            <Dialog
-                                open={open}
-                                onClose={handleClose}
-                                aria-labelledby="alert-dialog-title"
-                                aria-describedby="alert-dialog-description"
-                            >
-                                <DialogTitle id="alert-dialog-title">
-                                    <Typography variant="h6" my={2}>
-                                        Zvolená pololožka bola pridaná do košíka.
-                                    </Typography>
-                                </DialogTitle>
-                                <DialogActions style={{justifyContent: 'space-between'}}>
-                                    <Button onClick={handleClose}>Pokračovať v nákupe</Button>
-                                    <Button
-                                        onClick={handleToCart}
-                                        endIcon={<ShoppingCart/>}
-                                        autoFocus
-                                    >
-                                        Do košíka
-                                    </Button>
-                                </DialogActions>
-                            </Dialog>
-                        </Box>
-                    </Grid>
-                </Grid>
+                    }
+                    />
+                  <FormControlLabel
+                    value="25"
+                    control={<Radio />}
+                    label={
+                        <Typography>
+                        25cm (25-30 porcií)  <i>+50€</i>
+                        </Typography>
+                    }
+                    />
+                </RadioGroup>
+                </Typography>
+              </FormControl>
             </Box>
-        </Box>
-    )
+            <Box>
+              <FormControl>
+                <Typography variant="h6" my={1}>
+                  Príchuť
+                </Typography>
+                <RadioGroup
+                  name="controlled-radio-buttons-group"
+                  value={flavour}
+                  onChange={handleFlavourChange}
+                >
+                  <FormControlLabel
+                    value="chocolate"
+                    control={<Radio />}
+                    label="čokoládová"
+                  />
+                  <FormControlLabel
+                    value="vanilla"
+                    control={<Radio />}
+                    label="vanilková"
+                  />
+                  <FormControlLabel
+                    value="strawberry"
+                    control={<Radio />}
+                    label="jahodová"
+                  />
+                  <FormControlLabel
+                    value="coffee"
+                    control={<Radio />}
+                    label="kávová"
+                  />
+                </RadioGroup>
+                {flavourError && (
+                    <Typography variant="body2" color="error">
+                        Vyberte príchuť pred pridaním do košíka.
+                    </Typography>
+                )}
+              </FormControl>
+            </Box>
+            <Restrictions restrictions={restrictions}/>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              m={2}
+            >
+              <Typography variant="h6" my={2}>
+                Cena: {finalPrice}€
+              </Typography>
+              <Button
+                variant="contained"
+                endIcon={<ShoppingCartOutlined />}
+                onClick={handleClickOpen}
+              >
+                {' '}
+                Kúpiť
+              </Button>
+              <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+              >
+                  <DialogTitle id="alert-dialog-title">
+                      <Typography variant="h6" my={2}>
+                      Zvolená pololožka bola pridaná do košíka.
+                      </Typography>
+                  </DialogTitle>
+                  <DialogActions style={{ justifyContent: 'space-between' }}>
+                      <Button onClick={handleClose}>Pokračovať v nákupe</Button>
+                      <Button
+                        onClick={() => {
+                            handleClose();
+                            router.push('/kosik');
+                        }}
+                        endIcon={<ShoppingCart />}
+                        autoFocus
+                      >
+                      Do košíka
+                      </Button>
+                  </DialogActions>
+              </Dialog>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  )
 }
 
 export default CakeDetail
--- frontend/pages/ponuka/index.tsx
+++ frontend/pages/ponuka/index.tsx
@@ -2,42 +2,103 @@ import * as React from 'react';
 import { Autocomplete, Box, Button, Card, CardActionArea, CardContent, CardMedia, Chip, Grid, TextField, Typography, Stack } from '@mui/material'
 import { Torta } from "../../components/torta/Torta"
 
-// filter
-// const category = [
-//   { title: 'Narodeninová'},
-//   { title: 'Svadobná'}
-// ];
+const category = [
+  { title: 'Narodeninová'},
+  { title: 'Svadobná'},
+  { title: 'Iné'}
+];
+const restriction  = [
+  { title: 'Bez laktózy'},
+  { title: 'Bez lepku'},
+  { title: 'Vegánska'}
+];
 
 const cakes = [
-  { id: 0, name: 'Makrónková torta', description: 'Torta s lahodnými makrónkami, čokoládovou polevou a maslovým krémom.', price: 40 },
-  { id: 1, name: 'Čokoládová torta', description: 'Čokoládová torta s bohatou chuťou, dozdobená kúskami čokolády a jedlým zlatom.', price: 35},
-  { id: 2, name: 'Narodeninová torta', description: 'Minimalistická narodeninová torta pre dvoch s farebnou posýpkou.', price: 25 },
-  { id: 3, name: 'Torta Jednorožec', description: 'Torta Jednorožec zdobená farebným krémom a dominantným zlatým rohom.', price: 55 },
+  { id: 0, name: 'Makrónková torta', description: 'Torta s lahodnými makrónkami, čokoládovou polevou a maslovým krémom.', price: 40, category:'Iné',  restriction:[]  },
+  { id: 1, name: 'Čokoládová torta', description: 'Čokoládová torta s bohatou chuťou, dozdobená kúskami čokolády a jedlým zlatom.', price: 35, category:'Narodeninová',  restriction:['Bez lepku'] },
+  { id: 2, name: 'Narodeninová torta', description: 'Minimalistická narodeninová torta pre dvoch s farebnou posýpkou.', price: 25, category:'Narodeninová',  restriction:['Bez laktózy', 'Vegánska']  },
+  { id: 3, name: 'Torta Jednorožec', description: 'Torta Jednorožec zdobená farebným krémom a dominantným zlatým rohom.', price: 55, category:'Iné',  restriction:['Bez laktózy', 'Bez lepku', 'Vegánska']  },
+  { id: 4, name: 'Dinosaurusová torta', description: 'Torta Jednorožec zdobená farebným krémom a dominantným zlatým rohom.', price: 55, category:'Iné',  restriction:['Bez laktózy', 'Bez lepku', 'Vegánska'] },
+  { id: 5, name: 'Svadobná', description: '...', price: 0, category:'Svadobná', restriction:[] },
+  { id: 6, name: 'Svadobná', description: '...', price: 0, category:'Svadobná', restriction:['Vegánska']  },
 ];
 
 export default function Ponuka() {
+  const [selectedCategories, setSelectedCategories] = React.useState([]);
+  const [selectedRestrictions, setSelectedRestrictions] = React.useState([]);
+  const [filteredCakes, setFilteredCakes] = React.useState(cakes);
+
+  const handleCategoryChange = (event, newCategories) => {
+    setSelectedCategories(newCategories);
+  };
+  const handleRestrictionChange = (event, newRestrictions) => {
+    setSelectedRestrictions(newRestrictions);
+  };
+
+  React.useEffect(() => {
+    var updatedCakes = selectedCategories.length === 0
+      ? cakes
+      : cakes.filter(cake => selectedCategories.map(category => category.title).includes(cake.category))
+      
+    updatedCakes = selectedRestrictions.length === 0
+      ? updatedCakes
+      : updatedCakes.filter(cake => selectedRestrictions.some(restriction => cake.restriction.includes(restriction.title)))
+
+    setFilteredCakes(updatedCakes);
+  }, [selectedCategories, selectedRestrictions]);
+
   return (
-    <Box my={5}>
-      {/* <Stack spacing={3} sx={{ width: 450 }} my={3} display="flex"> // TODO filter
-        <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={category}
-          getOptionLabel={(option) => option.title}
-          filterSelectedOptions
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="filtuj kategóriu torty"
-              placeholder="kategória torty"
+    <Box marginBottom={3}>
+      <Grid container justifyContent="center" borderBottom={1}>
+        <Box mx={1}>
+          <Typography display="inline-block">Kategória: </Typography>
+          <Stack spacing={3} sx={{ width: 380 }} my={2} mx={1} display="inline-block">
+            <Autocomplete
+              multiple
+              id="tags-outlined"
+              options={category}
+              getOptionLabel={(option) => option.title}
+              filterSelectedOptions
+              onChange={handleCategoryChange}
+              renderInput={(params) => (
+                <Typography variant="h4">
+                  <TextField
+                    {...params}
+                    label="filtuj kategóriu torty"
+                    placeholder="kategória torty"
+                  />
+                </Typography> 
+              )}
             />
-          )}
-        />fdx
-      </Stack> */}
+          </Stack>
+        </Box>
+        <Box mx={1}>
+          <Typography display="inline-block">Obmedzenia: </Typography>
+          <Stack spacing={3} sx={{ width: 380 }} my={2} mx={1} display="inline-block">
+            <Autocomplete
+              multiple
+              id="tags-outlined"
+              options={restriction}
+              getOptionLabel={(option) => option.title}
+              filterSelectedOptions
+              onChange={handleRestrictionChange}
+              renderInput={(params) => (
+                <Typography variant="h4">
+                  <TextField
+                    {...params}
+                    label="filtuj obmedzenia torty"
+                    placeholder="obmedzenia"
+                  />
+                </Typography> 
+              )}
+            />
+          </Stack>
+        </Box>
+      </Grid>
       <Grid container spacing={{ xs: 3, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      {cakes.map((cake) => (
-        <Grid item xs={4} sm={4} md={4} key={cake.id}>
-          <Torta id={cake.id} name={cake.name} description={cake.description} price={cake.price}/>
+      {filteredCakes.map((cake) => (
+        <Grid marginTop={3} item xs={4} sm={4} md={4} key={cake.id}>
+          <Torta id={cake.id} name={cake.name} description={cake.description} price={cake.price} restrictions={cake.restriction}/>
         </Grid>
       ))}
       </Grid>
